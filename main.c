20bc0451054248528dd34ed218405afa48cc60ad
Charles Adair <charlie@adair.sh>
2026-08-30 21:38:46 -0600

    Implemented some primitaves: - The ability to split numbers into two halves (ex [1234] -> [12 , 34]) - The ability to multiply by 10 without use of * operator

--- main.c
+++ main.c
@@ -1,8 +1,89 @@
 // This needs to be an implementation of the Split-Multiply algorithm
+// Constraints: 1≤𝑛≤20000; n is also guaranteed to be a power of 2
+//    x and y are both positive numbers of the same length.
+
 #include <stdio.h>
 
 
 int main(void) {
-  printf("hello world!\n");
+  printf("hello world2!\n");
+
   return 0;
 }
+
+int multiply_by_ten(int input) {
+  int output;
+  output = (input << 3); // Multiply input by eight
+  output += (input << 1); // Add input times 2
+
+  return output;
+}
+
+// Find how many digits in input
+int num_digits(int input) {
+  int numDigits = 1; // Assume digit is at least 1
+  int tmp = input % 10;
+  while (tmp / 10 > 1) {
+    tmp = tmp % 10;
+    numDigits++;
+  }
+  return numDigits;
+}
+
+typedef struct {
+  int head;
+  int tail;
+} Tuple;
+
+// Given a number, split it into its
+// front n/2 digits and back n/2 digits
+Tuple split_number(int input) {
+
+  int numDigits = num_digits(input);
+
+  Tuple output;
+  output.head = 0;
+  output.tail = 0;
+
+  if (numDigits == 1) {
+    output.tail = input;
+    return output;
+  }
+
+  int tmp = input;
+  // pop the last n/2 digits
+  // by dividing by 10
+  for (int i = 0; i < (numDigits / 2); i++) {
+    
+    int addToTail = tmp % 10;
+    tmp /= 10;
+
+    // Multiply by 10 based on digit position
+    for (int j = 0; j < i; i++) {
+      addToTail = multiply_by_ten(addToTail);
+    }
+
+    output.tail += addToTail;
+  }
+
+  output.head = tmp;
+
+  return output;
+}
+
+int split_multiply(int x, int y) {
+  // Base case: numbers are single digit
+  int newX, newY = 0;
+  if (x / 10.0 > 1) {
+    // Don't do anything to x
+    newX = x;
+  }
+  else {
+    int i = 0;
+  }
+  if (y / 10.0 > 1) {
+    newY = y;
+  }
+  return 0;
+}
+
